da6e40228229ce7465891b5b7c6b0dd2744da2e1
protonu1122 <protonu1122@onmail.com>
2022-03-25 21:49:39 -0700

    removing output changeng

--- main.py
+++ main.py
@@ -7,19 +7,19 @@ def getYtFileName(ytLink):
 
 
 def upload(fileName):
-    cmd = './rclone copy "~/output/{}" GDrive:'.format(fileName)
+    cmd = './rclone copy "{}" GDrive:'.format(fileName)
     print("upload " + cmd)
     return str(os.popen(cmd).read())
 
 
 def getDownloadLink(fileName):
-    cmd = './rclone link GDrive:"~/output/{}"'.format(fileName)
+    cmd = './rclone link GDrive:"{}"'.format(fileName)
     print("getDownloadLink ", cmd)
     return str(os.popen(cmd).read())
 
 
 ytLink = input("Youtube: ")
-cmd = 'python3 yt-dlp -o "~/output/(title)s.%(ext)s" -f b {}'.format(ytLink)
+cmd = 'python3 yt-dlp -o "(title)s.%(ext)s" -f b {}'.format(ytLink)
 print(cmd)
 output = os.popen(cmd).read()
 print("Done!")
@@ -28,6 +28,6 @@ print("Upload Done!")
 print("Uploaded as " + getYtFileName(ytLink))
 print("Download here " + getDownloadLink(getYtFileName(ytLink)))
 try:
-    os.remove("~/output/" + getYtFileName(ytLink))
+    os.remove(getYtFileName(ytLink))
 except:
     print("The file does not exist")
