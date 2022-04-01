2bdf829e5ba08c836f592b2b45c8c938a524f4d5
gouhund <protonu8899@onmail.com>
2022-04-01 04:31:13 -0700

    go install

--- autopush.sh
+++
@@ -1,5 +0,0 @@
-git add . -f
-echo -n "Commit: "
-read commit
-git commit -am $commit
-git push heroku master
--- functions.py
+++ functions.py
@@ -272,3 +272,4 @@ def vidInstaStream(instaLink):
     send = "https://streamable.com/" + cmdUpload["shortcode"]
     time.sleep(10)
     return send
+    #
