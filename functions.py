32509f1975e74eacf00afdbce37942884555a385
gouhund <protonu8899@onmail.com>
2022-04-02 20:26:11 -0700

    run_command

--- functions.py
+++ functions.py
@@ -3,6 +3,8 @@ import re
 import random
 from pystreamable import StreamableApi
 import time
+import subprocess
+import shlex
 
 streamable = StreamableApi("protonu1122@tutanota.com", "Protonuonmail12.")
 
